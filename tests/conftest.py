8fa85675f9ed32ed1a7523d25ff834ec8df22fb7
Rustam <kodzokoff2016@yandex.ru>
2024-02-13 08:03:28 +0300

    first

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+import pytest
+from selene import browser
+
+
+@pytest.fixture(scope="function", autouse=True)
+def browser_manage():
+    browser.config.base_url = 'https://demoqa.com/automation-practice-form'
+    browser.config.window_width = 1420
+    browser.config.window_height = 1080
+
+    yield
+
+    browser.quit()
